846503fc4cc20ee70628f0598f41d29ad7c3736b
VyDoan <vydoan3100301@gmail.com>
2025-02-05 16:03:36 +0700

    simple AtGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,13 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
 import { JwtModule } from '@nestjs/jwt';
 import { MailModule } from '../mail/mail.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [JwtModule.register({}), MailModule, forwardRef(() => UsersModule)],
+  imports: [JwtModule.register({}), MailModule],
   providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
   controllers: [AuthController],
-  exports: [AuthService],
 })
 export class AuthModule {}
